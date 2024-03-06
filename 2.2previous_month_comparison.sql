5eaf959894fd5333aa9719076f5054b66d6ae2d4
cindy17656 <141680442+cindy17656@users.noreply.github.com>
2024-03-06 04:50:06 -0800

    Add files via upload

---
+++ 2.2previous_month_comparison.sql
@@ -0,0 +1,49 @@
+-- After creating tables, I directly import my processed data(csv file attaced)
+
+-- How does the ranking of the top 5 brands by receipts scanned for the recent month 
+-- compare to the ranking for the previous month?
+
+
+with items_with_brand as (
+	select
+	 p.name
+	 ,p.brand_id
+	 ,ri.receipt_id
+	from products as p
+	join receipt_items as ri
+	 on p.barcode = ri.barcode or p.barcode = ri.userFlaggedBarcode
+),
+brand_date as(
+	select
+	 ib.name
+	 , ib.brand_id
+	 , ib.receipt_id
+	 , r.datescanned 
+	from items_with_brand as ib
+	left join receipts as r
+	 on ib.receipt_id = r.receipt_id
+),
+ranked_month as(
+	select
+	 name
+	, count(distinct receipt_id) brand_count
+	, rank() over(partition by name order by max(date_trunc('month', datescanned)) desc) as past_n_month
+	from brand_date
+	group by name
+),
+ranked_brand as(
+	select 
+	 name
+	,past_n_month
+	,rank() over(partition by past_n_month order by brand_count desc) as top_n_brand
+	from ranked_month
+)
+select
+ r1.name
+ ,r1.top_n_brand as past_1_month_rank_for_top5_brand
+ ,r2.top_n_brand as previous_month_rank_comparison
+
+from ranked_brand as r1
+left join ranked_brand as r2
+on r1.name = r2.name and r1.past_n_month +1  = r2.past_n_month
+where r1.top_n_brand <=5
